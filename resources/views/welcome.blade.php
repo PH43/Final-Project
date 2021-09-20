fde47257e6dcdf54d2222a4d58bfb9eab1368f4c
Quang Tran H <quang.tran@monstar-lab.com>
2021-09-20 21:09:30 +0700

    edit route for login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel PH43 Final
                 </div>
 
                 <div class="links">
